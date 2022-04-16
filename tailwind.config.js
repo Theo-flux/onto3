18c199c31ccf54c6a1096c9e5ea89290102df920
Ekunnusi Theophilus <ekunnusitheophilus@gmail.com>
2022-04-16 01:27:48 +0100

    tailwindcss configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        museo: ["museo", ...defaultTheme.fontFamily.sans],
+        sans: ["museo", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 }
