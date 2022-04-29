65c38ce9a25e75af54132170c0d2d7c12abcb17d
Ekunnusi Theophilus <ekunnusitheophilus@gmail.com>
2022-04-29 10:52:40 +0100

    app config set-up

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: "var(--color-black)",
+      white: "var(--color-white)",
+      green: "var(--color-green)",
+      bunker: "var(--color-bunker)",
+    },
+
+    fontFamily: {
+      oxanium: ['oxanium', defaultTheme.fontFamily.sans],
+      neue: ['neue', defaultTheme.fontFamily.sans]
+    },
     extend: {},
   },
   plugins: [],
