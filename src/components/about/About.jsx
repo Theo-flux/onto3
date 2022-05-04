56ef6d319289aef0e300b18f00da6fc6b9d0004d
Ekunnusi Theophilus <ekunnusitheophilus@gmail.com>
2022-05-04 15:28:04 +0100

    tag added

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,23 +1,23 @@
 import React from "react";
-import { Section, Text, Title, Button, Div } from "../../shared";
+import { Section, Text, Title, Tag, Div } from "../../shared";
 import nft from "../../images/nft.png";
 
 function About() {
   return (
     <Section id="about" className={""}>
      <Div>
-        <div className="flex flex-col md:flex-row justify-between mb-10">
+        <div className="flex flex-col md:flex-row justify-between">
                 <div className="w-full md:w-[45%]">
-                    <Title className="mt-5"> <span className="font-thin">On-Chain Resume</span> <br /> New</Title>
-                    <Text className='mt-4'>
+                    <Tag>Our story</Tag>
+                    <Title className="mb-8">About us</Title>
+                    <Text className=''>
                         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio ullam magnam iure ratione? Dolorum molestiae iusto ducimus officiis hic quia non expedita at? Vero quam ratione, explicabo velit modi totam!</p> 
                     
                         <p className="mt-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis facilis ratione quibusdam in earum similique quos, voluptatem officia accusantium eveniet sed aliquid atque non repellendus dolorum. Praesentium vitae obcaecati asperiores.</p>
-                        <p className="mt-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis facilis ratione quibusdam in earum similique quos, voluptatem officia accusantium eveniet sed aliquid atque non repellendus dolorum. Praesentium vitae obcaecati asperiores.</p>
                     </Text>
                 </div>
-                <div className="w-full md:w-[40%] border border-green mt-8 md:m-0">
-                    <img src={nft} alt="" className="w-[100%] object-fit"  />
+                <div className="w-full md:w-[40%] mt-8 md:m-0">
+                    <img src={nft} alt="" className="w-[100%] border border-green object-fit"  />
                     <div className="flex">
                         <div></div>
                         <div></div>
