f815a62c1335a2973b20b93decbd42bba91b568a
taiwoj121 <taiwoj121@gmail.com>
2022-05-05 16:19:15 +0100

    revamped about design

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,44 +1,41 @@
 import React from "react";
 import { Section, Text, Title, Button, Div } from "../../shared";
 import nft from "../../images/nft.png";
+import about from "../../images/about-2.png"
 
 function About() {
   return (
     <Section className={""}>
      <Div>
-        <div className="flex flex-col md:flex-row justify-between mb-10">
-          <div className="w-full md:w-[45%]">
-              <Title className="mt-5"> <span className="font-thin">On-Chain Resume</span> <br /> New</Title>
-              <Text className='mt-4'>
-                  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio ullam magnam iure ratione? Dolorum molestiae iusto ducimus officiis hic quia non expedita at? Vero quam ratione, explicabo velit modi totam!</p> 
-              
-                  <p className="mt-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis facilis ratione quibusdam in earum similique quos, voluptatem officia accusantium eveniet sed aliquid atque non repellendus dolorum. Praesentium vitae obcaecati asperiores.</p>
-                  <p className="mt-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis facilis ratione quibusdam in earum similique quos, voluptatem officia accusantium eveniet sed aliquid atque non repellendus dolorum. Praesentium vitae obcaecati asperiores.</p>
-              </Text>
+        <div className="flex flex-col md:flex-row justify-between mb-10 mt-4 items-center">
+          <div>
+            <Title className="inline-block">About</Title> <span className="text-white ml-3 ">ONTO3 <span className="text-green border-b-2">APP</span></span>
           </div>
-          <div className="w-full md:w-[40%] border border-green mt-8 md:m-0">
-              <img src={nft} alt="" className="w-[100%] object-fit"  />
-              <div className="flex">
-                  <div></div>
-                  <div></div>
-              </div>
+          <div>
+             <span className="text-white text-lg font-thin"><span className="text-5xl font-bold">.</span> June</span>
+             <span className="font-bold text-white block ml-5">2022</span>
           </div>
         </div>
-        <div className="flex mt-4 justify-between space-x-8">
-          <div className="border border-gray w-50 ">
-            <p className="text-white p-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur eveniet, accusamus iure enim debitis illo exercitationem similique quo? Distinctio tenetur facilis dolore expedita cum ut quia quod. Praesentium, atque. Veritatis!</p>
+        <div className="text-white text-center w-full mx-auto max-w-2xl">
+            <p className="">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt soluta ratione dolor quisquam numquam ipsam illum consequatur necessitatibus dicta exercitationem nihil voluptatibus, error similique impedit, velit perferendis tempora, beatae accusamus!
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam molestiae recusandae impedit in, quaerat iusto commodi culpa odio facilis deleniti ex soluta. Corrupti laboriosam aut quas vero dolorem natus facilis?
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit quae ipsam et aliquam itaque nobis quas odit rem nihil similique nam est exercitationem fugit, magni vel corporis. Molestiae, atque fugit!
+            </p>
           </div>
-          <div className="border border-gray w-50">
-            <p className="text-white p-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur eveniet, accusamus iure enim debitis illo exercitationem similique quo? Distinctio tenetur facilis dolore expedita cum ut quia quod. Praesentium, atque. Veritatis!</p>
-          </div>
-          <div className="border border-gray w-50">
-            <p className="text-white p-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur eveniet, accusamus iure enim debitis illo exercitationem similique quo? Distinctio tenetur facilis dolore expedita cum ut quia quod. Praesentium, atque. Veritatis!</p>
-          </div>
-        </div>
      </Div>
      <Div>
-       <div>
-       <Title className="mt-5 text-center"> </Title>
+       <div className="flex justify-between">
+         <div className="">
+           <h3 className="text-2xl text-white">About <span className="text-green">Us</span></h3>
+           <p className="text-white mt-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos voluptatem laborum in accusamus sed nihil repellendus illum modi consequatur. Officia, quas sequi. Quis asperiores porro repudiandae alias, dicta vitae totam?</p>
+          </div>
+         <div>
+           <img src={about} alt="" />
+         </div>
+         <div className="ml-8">
+           <h3 className="text-2xl text-white">Why <span className="text-green">Us</span></h3>
+           <p className="text-white mt-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos voluptatem laborum in accusamus sed nihil repellendus illum modi consequatur. Officia, quas sequi. Quis asperiores porro repudiandae alias, dicta vitae totam?</p>
+          </div>
        </div>
      </Div>
     </Section>
