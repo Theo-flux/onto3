0d71bfc9729418701ca2cd27c95c780b6dd43cd2
Ekunnusi Theophilus <ekunnusitheophilus@gmail.com>
2022-05-13 17:56:52 +0100

    vide embedded

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { Footer, Home, Navbar, About, AppComing, Faq } from '../src/components';
+import { Footer, Home, Navbar, About, Video, AppComing, Faq } from '../src/components';
 import "./assets/css/onto3.css";
 
 function App() {
@@ -9,6 +9,7 @@ function App() {
       <Navbar/>
       <Home/>
       <About/>
+      <Video/>
       <AppComing/>
       <Faq/>
       <Footer/>
--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,7 +1,8 @@
 export { default as Navbar } from "./navbar/Navbar";
 export { default as Footer } from "./footer/Footer";
 export { default as Home } from "./home/Home";
-export { default as  About} from "./about/About";
+export { default as  About } from "./about/About";
 export { default as Roadmap } from "./roadmap/Roadmap";
 export { default as AppComing } from "./appComing/AppComing";
 export { default as Faq } from "./faq/Faq";
+export { default as Video } from "./video/Video";
